a49874e2b896cee49124714e25810b4717bed056
edy <edyehl15@gmail.com>
2024-03-02 23:30:35 -0600

    primer_commit

---
+++ clase5.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+main(){
+	/*
+	int i=0;
+	while(i<5){
+		cout<<"while: "<<i<<endl;
+		i++;
+	}*/
+	
+	char respuesta='s' ;
+	while(respuesta=='s'|| respuesta=='s' ){
+		cout<<"desea ingresar otro valor (s/n) ";
+		cin>>respuesta;
+	}
+
+	system("pause");
+}
+
